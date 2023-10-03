8c8b635d19b232bf6d08c50fd3beda72b4e2d736
mohammadali sunasara <102787794+mohammadali10-12@users.noreply.github.com>
2023-10-03 12:02:14 +0000

    conversion of data type

---
+++ 01_basics/conversion.js
@@ -0,0 +1,27 @@
+let account_id = 12376
+
+console.log(typeof account_id);
+
+// convert number to string
+
+let string_number = String(account_id);
+console.log(typeof string_number);
+
+// convert string in boolean
+let string_boolean = 'test';
+let boolean = Boolean(string_boolean);
+console.log(boolean);
+
+// convert boolean to number
+
+let boolean_value = true;
+let number = Number(boolean_value);
+console.log(typeof number);
+
+let unique_value = "23abc";
+
+let in_number = Number(unique_value);
+console.log(in_number);
+
+let in_string = String(unique_value);
+console.log(typeof in_string);
